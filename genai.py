4a26de5d56e04815fa347377ad4d2b239c1ca830
wilsoncliffordkwame-oss <wilsoncliffordkwame>
2025-10-21 19:45:06 +0000

    Learnt about pattern recognition using ngrams from NLTK 2

--- genai.py
+++ genai.py
@@ -7,6 +7,15 @@ nltk.download('punkt')
 
 sentence = 'I am learning Generative AI'
 tokens = word_tokenize(sentence)
-bigrams = list(ngrams(tokens, 2))
 
-print(bigrams)
+# Unigram
+unigrams = list(ngrams(tokens, 1))
+print('Unigrams:', unigrams)
+
+# Bigram
+bigrams = list(ngrams(tokens, 2))
+print('Bigrams:', bigrams)
+
+# Trigram
+trigrams = list(ngrams(tokens, 3))
+print('Trigrams:', trigrams)
